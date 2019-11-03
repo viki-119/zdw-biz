f45be5abe763f511ba821b34b3de974db73614bc
viki-119 <1192756137@qq.com>
2019-11-03 20:40:17 +0800

    feat: add react-transition-group

--- demo/component/test/child.js
+++ demo/component/test/child.js
@@ -2,6 +2,10 @@ import React, {Component, PureComponent} from 'react';
 import PropType from 'prop-types';
 
 export default class Child extends PureComponent {
+  // componentDidCatch(err, info) {
+  //   console.log('ddddd', err, info)
+  // }
+
   render() {
     console.log('Child render');
     const { person } = this.props;
--- demo/component/test/father.js
+++ demo/component/test/father.js
@@ -1,6 +1,8 @@
 import React, {Component, PureComponent} from 'react';
 import PropType from 'prop-types';
-import Child from './child'
+import {CSSTransition, TransitionGroup,} from 'react-transition-group';
+import Child from './child';
+import './style.less';
 
 export default class Father extends PureComponent {
   constructor() {
@@ -8,9 +10,16 @@ export default class Father extends PureComponent {
     this.state = {
       person: {
         name: 'rrrr'
-      }
+      },
+      age: 1,
+      enable: true,
+      enableB: false,
     };
     console.log('constructor');
+  }
+
+  componentDidMount() {
+    // document.getElementById('pattern').addEventListener('mouseleave', this.mouseleave)
   }
 
   // 这种写法和constructor 的写法是一样的,如果constructor中定义了this.state, 则下面这种写法将失效
@@ -34,21 +43,62 @@ export default class Father extends PureComponent {
   //   console.log('ddddd', this.state)
   // }
 
+  onMouseOver = () => {
+    console.log('onMouseOver')
+    this.setState({
+      enable: false,
+      enableB: true,
+    })
+  }
+
+  onMouseOut = () => {
+    console.log('onMouseOut')
+    this.setState({
+      enable: true,
+      enableB: false,
+    })
+  }
+
   changeState = () => {
     let { person } = this.state;
     person.name = 'sxt2';
     this.setState({
-      // person,
-      person: {...person}
+      person,
+      // person: {...person}
+      age: '1', // 这里的浅比较用的是=== 而非==
     })
   };
   render() {
     console.log('father render');
-    const { person } = this.state;
+    const { person, enable, enableB } = this.state;
     return (
-      <div>
+      <div id='father'>
         <button onClick={this.changeState}>点击</button>
         <Child person={person} />
+        <div id='pattern' onMouseOver={this.onMouseOver}
+        onMouseLeave={this.onMouseOut} className="pattern">
+          <CSSTransition
+            // 组件名，提供css选择器使用，必填参数，如果缺少了，程序会崩溃报错，注意这个参数带's'
+            classNames='animation'
+            // 组件是否展示,true为显示
+            in={enable}
+            // 动画持续时间，需要和css的设置保持一致
+            timeout={1000}
+            // 当in的属性变为false之后，卸载组件，过程可设置动画
+            unmountOnExit
+          >
+            {/*CSSTransition的子组件，必须是个函数，或者组件*/}
+            <div>开奖号码</div>
+          </CSSTransition>
+          <CSSTransition
+            classNames='animation'
+            in={enableB}
+            timeout={1000}
+            unmountOnExit
+          >
+            <div className='enableB'>未中奖</div>
+          </CSSTransition>
+        </div>
       </div>
     );
   }
